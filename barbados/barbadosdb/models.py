6b3bf142f59191538b77208769fc3c2cf2dbb87f
Markus Törnqvist <markus.tornqvist@codento.com>
2015-12-08 16:57:21 +0200

    Harbour / Jetty / Berth
    Consider trigram indexes for Harabour(name) maybe later

--- barbados/barbadosdb/models.py
+++ barbados/barbadosdb/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+
+
+class Harbour(models.Model):
+    """The topmost model
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=64, db_index=True)
+
+    class Meta:
+        app_label = 'barbadosdb'
+
+
+class Jetty(models.Model):
+    """Jetties in the Harbours
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    harbour = models.ForeignKey(Harbour)
+
+    name = models.CharField(max_length=2, db_index=True)
+
+    class Meta:
+        app_label = 'barbadosdb'
+        unique_together = ('harbour', 'name')
+
+
+class Berth(models.Model):
+    """Berths of the Jetties
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    jetty = models.ForeignKey(Jetty)
+
+    name = models.CharField(max_length=5, db_index=True)
+
+    class Meta:
+        app_label = 'barbadosdb'
+        unique_together = ('jetty', 'name')
+
--- barbados/settings.py
+++ barbados/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'pipeline',
     'django_extensions',
+    'barbados.barbadosdb',
 )
 
 
