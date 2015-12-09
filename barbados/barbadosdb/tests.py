9d1f5dce724c0044f91f5b6ff49cdf4382dae6af
Markus Törnqvist <markus.tornqvist@codento.com>
2015-12-09 14:51:01 +0200

    Simple pytest test

--- barbados/barbadosdb/tests.py
+++ barbados/barbadosdb/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+# vim: fenc=utf-8
+
+from . import models
+
+import pytest
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+def test_bad_phone_number():
+    __tracebackhide__ = True
+
+    try:
+        u = models.User(username='jkala',
+                        email='james.kala@example.com',
+                        phone_number='123')
+
+        u.full_clean()
+
+        pytest.fail(msg='User should not be valid')
+    except models.ValidationError:
+        pass
+
