53faa09e2f87c785ed19df095fc2a439151d353c
Markus Törnqvist <markus.tornqvist@codento.com>
2015-12-10 11:57:57 +0200

    Static urls

--- barbados/urls.py
+++ barbados/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 
-# from django.conf.urls import include
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.conf.urls import url
+from django.conf import settings
 from django.contrib import admin
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # url(r'^admin_tools/', include('admin_tools.urls')),
-]
+] + static(settings.STATIC_URL)
 
