371d992dc1a63ad3b5e24e18e8921efe18ccc1f8
Markus Törnqvist <markus.tornqvist@codento.com>
2015-12-10 12:04:54 +0200

    Register the User model

--- barbados/barbadosdb/admin.py
+++ barbados/barbadosdb/admin.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """Django cannot find our custom user by default
+    """
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('birth_date', 'first_name', 'last_name',
+                                        'email', 'phone_number',
+                                        'street_address', 'city', 'country_code')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
