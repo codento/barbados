4d5e34521c12bf1876117859e9414461a255b985
David Price <david.price@codento.com>
2015-12-22 16:38:38 +0200

    Add club auth tests and missing post auth tests

--- barbados/barbadosweb/api_auth_tests.py
+++ barbados/barbadosweb/api_auth_tests.py
@@ -132,10 +132,94 @@ def test_ordinary_user_get_other(ordinary_user, other_ordinary_user):
 
 
 @pytest.mark.django_db()
+def test_unauthenticated_list_users():
+    client = APIClient()
+
+    response = client.get('/api/user/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
 def test_unauthenticated_get_user(ordinary_user):
     client = APIClient()
 
     response = client.get('/api/user/' + str(ordinary_user.id) + '/')
+    assert response.status_code == 403
+
+
+# POST
+
+@pytest.mark.django_db()
+def test_harbourmaster_post_user(harbourmaster_user):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.post('/api/user/', {
+        'username': 'johndoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'password': 'secret',
+        'city': 'Helsinki'
+    }, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_secretary_post_user(secretary_user):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.post('/api/user/', {
+        'username': 'johndoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'password': 'secret',
+        'city': 'Helsinki'
+    }, format='json')
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db()
+def test_committee_member_post_user(committee_member_user):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.post('/api/user/', {
+        'username': 'johndoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'password': 'secret',
+        'city': 'Helsinki'
+    }, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_post_user(ordinary_user):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.post('/api/user/', {
+        'username': 'johndoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'password': 'secret',
+        'city': 'Helsinki'
+    }, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_post_user():
+    client = APIClient()
+
+    response = client.post('/api/user/', {
+        'username': 'johndoe',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'password': 'secret',
+        'city': 'Helsinki'
+    }, format='json')
     assert response.status_code == 403
 
 
@@ -284,6 +368,14 @@ def test_ordinary_user_delete_other(ordinary_user, other_ordinary_user):
     response = client.delete('/api/user/' + str(other_ordinary_user.id) + '/')
     assert response.status_code == 403
 
+
+@pytest.mark.django_db()
+def test_unauthenticated_delete_user(ordinary_user):
+    client = APIClient()
+
+    response = client.delete('/api/user/' + str(ordinary_user.id) + '/')
+    assert response.status_code == 403
+
 # We have no special treatment for secretary or ordinary user deleting themselves
 
 
@@ -387,10 +479,111 @@ def test_ordinary_user_get_other_boat(ordinary_user, other_ordinary_user, boat):
 
 
 @pytest.mark.django_db()
+def test_unauthenticated_list_boats():
+    client = APIClient()
+
+    response = client.get('/api/boat/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
 def test_unauthenticated_get_boat(boat):
     client = APIClient()
 
     response = client.get('/api/boat/' + str(boat.id) + '/')
+    assert response.status_code == 403
+
+
+# POST
+
+@pytest.mark.django_db()
+def test_harbourmaster_post_boat(admin_user, harbourmaster_user, ordinary_user):
+    client = APIClient()
+    assert client.login(username=admin_user.username, password='password')
+
+    response = client.get('/api/user/' + str(ordinary_user.id) + '/')
+    user_content = json.loads(response.content.decode('utf-8'))
+    user_url = user_content['url']
+
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.post('/api/boat/', {
+        'user': user_url,
+        'name': 'African Queen'
+    }, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_secretary_post_boat(admin_user, secretary_user, ordinary_user):
+    client = APIClient()
+    assert client.login(username=admin_user.username, password='password')
+
+    response = client.get('/api/user/' + str(ordinary_user.id) + '/')
+    user_content = json.loads(response.content.decode('utf-8'))
+    user_url = user_content['url']
+
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.post('/api/boat/', {
+        'user': user_url,
+        'name': 'African Queen'
+    }, format='json')
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db()
+def test_committee_member_post_boat(admin_user, committee_member_user, ordinary_user):
+    client = APIClient()
+    assert client.login(username=admin_user.username, password='password')
+
+    response = client.get('/api/user/' + str(ordinary_user.id) + '/')
+    user_content = json.loads(response.content.decode('utf-8'))
+    user_url = user_content['url']
+
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.post('/api/boat/', {
+        'user': user_url,
+        'name': 'African Queen'
+    }, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_post_boat(admin_user, ordinary_user):
+    client = APIClient()
+    assert client.login(username=admin_user.username, password='password')
+
+    response = client.get('/api/user/' + str(ordinary_user.id) + '/')
+    user_content = json.loads(response.content.decode('utf-8'))
+    user_url = user_content['url']
+
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.post('/api/boat/', {
+        'user': user_url,
+        'name': 'African Queen'
+    }, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_post_boat(admin_user, ordinary_user):
+    client = APIClient()
+
+    assert client.login(username=admin_user.username, password='password')
+
+    response = client.get('/api/user/' + str(ordinary_user.id) + '/')
+    user_content = json.loads(response.content.decode('utf-8'))
+    user_url = user_content['url']
+
+    client.logout()
+
+    response = client.post('/api/boat/', {
+        'user': user_url,
+        'name': 'African Queen'
+    }, format='json')
     assert response.status_code == 403
 
 
@@ -613,5 +806,288 @@ def test_ordinary_user_delete_boat(ordinary_user, boat):
     assert client.login(username=ordinary_user.username, password='password')
 
     response = client.delete('/api/boat/' + str(boat.id) + '/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_delete_boat(boat):
+    client = APIClient()
+
+    response = client.delete('/api/boat/' + str(boat.id) + '/')
+    assert response.status_code == 403
+
+
+# Club tests
+# GET
+
+@pytest.mark.django_db()
+def test_harbourmaster_list_clubs(harbourmaster_user):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.get('/api/club/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_harbourmaster_get_club(harbourmaster_user, club):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.get('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_secretary_list_clubs(secretary_user):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.get('/api/club/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_secretary_get_club(secretary_user, club):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.get('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_committee_member_list_clubs(committee_member_user):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.get('/api/club/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_committee_member_get_club(committee_member_user, club):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.get('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_list_clubs(ordinary_user):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.get('/api/club/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_get_club(ordinary_user, club):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.get('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_list_clubs():
+    client = APIClient()
+
+    response = client.get('/api/club/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_get_club(club):
+    client = APIClient()
+
+    response = client.get('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 403
+
+
+# POST
+
+@pytest.mark.django_db()
+def test_harbourmaster_post_club(harbourmaster_user):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.post('/api/club/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_secretary_post_club(secretary_user):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.post('/api/club/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_committee_member_post_club(committee_member_user):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.post('/api/club/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_post_club(ordinary_user):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.post('/api/club/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_post_club():
+    client = APIClient()
+
+    response = client.post('/api/club/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+# PUT
+
+@pytest.mark.django_db()
+def test_harbourmaster_put_club(harbourmaster_user, club):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.put('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_secretary_put_club(secretary_user, club):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.put('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_committee_member_put_club(committee_member_user, club):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.put('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_put_club(ordinary_user, club):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.put('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthorized_put_club(club):
+    client = APIClient()
+
+    response = client.put('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+# PATCH
+
+@pytest.mark.django_db()
+def test_harbourmaster_patch_club(harbourmaster_user, club):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.patch('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_secretary_patch_club(secretary_user, club):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.patch('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_committee_member_patch_club(committee_member_user, club):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.patch('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_patch_club(ordinary_user, club):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.patch('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthorized_patch_club(club):
+    client = APIClient()
+
+    response = client.patch('/api/club/' + str(club.id) + '/', {'name': 'Some Club'}, format='json')
+    assert response.status_code == 403
+
+
+# DELETE
+
+@pytest.mark.django_db()
+def test_secretary_delete_club(secretary_user, club):
+    client = APIClient()
+    assert client.login(username=secretary_user.username, password='password')
+
+    response = client.delete('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_harbourmaster_delete_club(harbourmaster_user, club):
+    client = APIClient()
+    assert client.login(username=harbourmaster_user.username, password='password')
+
+    response = client.delete('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_committee_member_delete_club(committee_member_user, club):
+    client = APIClient()
+    assert client.login(username=committee_member_user.username, password='password')
+
+    response = client.delete('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_ordinary_user_delete_club(ordinary_user, club):
+    client = APIClient()
+    assert client.login(username=ordinary_user.username, password='password')
+
+    response = client.delete('/api/club/' + str(club.id) + '/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db()
+def test_unauthenticated_delete_club(club):
+    client = APIClient()
+
+    response = client.delete('/api/club/' + str(club.id) + '/')
     assert response.status_code == 403
 
